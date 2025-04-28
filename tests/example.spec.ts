ec224145c7cec8b7fc0148e8e8efb6b8b4bab0e8
Julie Laursen <julielaursen1@gmail.com>
2025-04-27 22:26:09 -0500

    get working test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,8 @@ import { test, expect } from "@playwright/test"
 test("has title", async ({ page }) => {
   await page.goto("https://julielaursen.github.io/")
 
-  await expect(page).toHaveTitle(/Julie Coleman/)
+  await expect(page).toHaveTitle(/My Portfolio/)
+  await expect(
+    page.getByRole("heading", { name: "Julie Coleman" })
+  ).toBeVisible()
 })
